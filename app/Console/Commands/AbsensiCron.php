ee5c5a7175773c147a91d993dd8ca7a9045deb98
kerjaanlucky <kerjaanlucky@gmail.com>
2024-07-04 21:37:51 +0700

    ok

---
+++ app/Console/Commands/AbsensiCron.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\AbsensiController;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class AbsensiCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'absensi:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Narik Data Absensi';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // Sample
+        $resullt = AbsensiController::fetchDaily();
+        if($resullt){
+            Log::info($resullt);
+        }else{
+            Log::info('gagal');
+        }
+    }
+}
--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -10,18 +10,17 @@ use Illuminate\Support\Facades\App;
 
 class AbsensiController extends BaseController
 {
-
     public function index(Request $request)
     {
         $perPage = $request->input('limit', 5000);
         $startDate = $request->input('start-date', Carbon::now());
-        $endDate = $request->input('end-date',Carbon::now());
+        $endDate = $request->input('end-date', Carbon::now());
 
         $data = Absensi::when($startDate && $endDate, function ($query) use ($startDate, $endDate) {
-                $startDate = Carbon::createFromFormat('Y-m-d', $startDate)->format('Y-m-d 00:00:00');
-                $endDate = Carbon::createFromFormat('Y-m-d', $endDate)->format('Y-m-d 23:59:59');
-                return $query->whereBetween('tanggal_data', [$startDate, $endDate]);
-            })
+            $startDate = Carbon::createFromFormat('Y-m-d', $startDate)->format('Y-m-d 00:00:00');
+            $endDate = Carbon::createFromFormat('Y-m-d', $endDate)->format('Y-m-d 23:59:59');
+            return $query->whereBetween('tanggal_data', [$startDate, $endDate]);
+        })
             // ->where('created_by', Auth::user()->id)
             ->orderBy('tanggal_data', 'desc')
             ->latest()
@@ -32,7 +31,6 @@ class AbsensiController extends BaseController
 
     public function getDataAbsensi(Request $request)
     {
-
         $start_date = $request->start_date;
         $form_date = $request->from_date;
 
@@ -45,7 +43,7 @@ class AbsensiController extends BaseController
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
@@ -55,17 +53,17 @@ class AbsensiController extends BaseController
 
     public function fetchAndStoreData(Request $request)
     {
-        $date = "2024-07-03";
+        $date = '2024-07-03';
         $existing = Absensi::whereDate('tanggal_data', $date)->get();
 
-        if($existing){
+        if ($existing) {
             foreach ($existing as $key => $value) {
                 $value->delete();
             }
         }
 
         $url = 'https://developer.fingerspot.io/api/get_attlog';
-        $data = '{"trans_id":"1", "cloud_id":"' . env('ABSEN_CLOUD_ID') . '", "start_date":"'.$date.'", "end_date":"'.$date.'"}';
+        $data = '{"trans_id":"1", "cloud_id":"' . env('ABSEN_CLOUD_ID') . '", "start_date":"' . $date . '", "end_date":"' . $date . '"}';
         $authorization = 'Authorization: Bearer ' . env('ABSEN_TOKEN');
 
         $ch = curl_init($url);
@@ -73,7 +71,7 @@ class AbsensiController extends BaseController
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
@@ -83,10 +81,7 @@ class AbsensiController extends BaseController
 
         $data = $final->data;
 
-
-
         if (isset($data)) {
-
             foreach ($data as $record) {
                 $existingRecord = Absensi::where('pin', $record->pin)
                     ->whereDate('jam_masuk', '=', date('Y-m-d', strtotime($record->scan_date)))
@@ -105,7 +100,7 @@ class AbsensiController extends BaseController
                         'jam_pulang' => $record->status_scan == 1 ? $record->scan_date : null,
                         'verify' => $record->verify,
                         'status_scan' => $record->status_scan,
-                        'tanggal_data' => $date
+                        'tanggal_data' => $date,
                     ]);
                 }
             }
@@ -123,7 +118,7 @@ class AbsensiController extends BaseController
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
@@ -133,7 +128,6 @@ class AbsensiController extends BaseController
 
     public function getAllPin()
     {
-
         $url = 'https://developer.fingerspot.io/api/get_all_pin';
         $data = '{"trans_id":"1", "cloud_id":"' . env('ABSEN_CLOUD_ID') . '", "start_date":"2024-07-03", "end_date":"2024-07-03"}';
         $authorization = 'Authorization: Bearer ' . env('ABSEN_TOKEN');
@@ -143,7 +137,7 @@ class AbsensiController extends BaseController
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
@@ -164,11 +158,73 @@ class AbsensiController extends BaseController
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
         curl_close($ch);
         return $result;
+    }
+
+    static function fetchDaily()
+    {
+        $date = Carbon::now();
+        try {
+            $existing = Absensi::whereDate('tanggal_data', $date)->get();
+            if ($existing) {
+                foreach ($existing as $key => $value) {
+                    $value->delete();
+                }
+            }
+
+            
+
+            $url = 'https://developer.fingerspot.io/api/get_attlog';
+            $data = '{"trans_id":"1", "cloud_id":"' . env('ABSEN_CLOUD_ID') . '", "start_date":"' . $date->format('Y-m-d') . '", "end_date":"' . $date->format('Y-m-d') . '"}';
+            $authorization = 'Authorization: Bearer ' . env('ABSEN_TOKEN');
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            $final = json_decode($result);
+
+            $data = $final->data;
+
+            if (isset($data)) {
+                foreach ($data as $record) {
+                    $existingRecord = Absensi::where('pin', $record->pin)
+                        ->whereDate('jam_masuk', '=', date('Y-m-d', strtotime($record->scan_date)))
+                        ->first();
+
+                    if ($existingRecord) {
+                        if ($record->status_scan == 1) {
+                            $existingRecord->jam_pulang = $record->scan_date;
+                        }
+                        $existingRecord->save();
+                    } else {
+                        Absensi::create([
+                            'pin' => $record->pin,
+                            'scan_date' => $record->scan_date,
+                            'jam_masuk' => $record->status_scan == 0 ? $record->scan_date : null,
+                            'jam_pulang' => $record->status_scan == 1 ? $record->scan_date : null,
+                            'verify' => $record->verify,
+                            'status_scan' => $record->status_scan,
+                            'tanggal_data' => $date,
+                        ]);
+                    }
+                }
+            }
+            return 'Data fetched and stored successfully';
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
